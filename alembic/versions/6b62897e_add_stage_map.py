2017bb9feb49bb638e7c256d8b4ed073621c331d
danielcmc100 <danielcmc100@gmail.com>
2026-05-28 13:39:06 -0300

    fix: update down_revision to correct migration reference

--- alembic/versions/6b62897e_add_stage_map.py
+++ alembic/versions/6b62897e_add_stage_map.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision: str = "6b62897e"
-down_revision: str | None = "c015cc994f32"
+down_revision: str | None = "7c3987160332"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
